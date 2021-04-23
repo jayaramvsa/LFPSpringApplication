9e20aeeb5686db020575e38dae02b4ca9faf0776
jayaramvsa <jayaramvsa@gmail.com>
2021-04-23 11:29:33 +0530

    Formatiing

--- src/main/java/com/lfp/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/lfp/myfirstapp/controller/HelloWorldController.java
@@ -7,25 +7,31 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/hello")
 public class HelloWorldController {
 
-    @RequestMapping(value = {"","/","/home"})
-    public String sayHello(){
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello() {
         return "Hello World!!!";
     }
 
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "firstName") String fName,
-                           @RequestParam(value = "lastName") String lName){
+                           @RequestParam(value = "lastName") String lName) {
         return "Hello " + fName + " " + lName + "!!";
     }
 
     @GetMapping("/param/{strName}")
-    public String sayHelloParam(@PathVariable String strName){
+    public String sayHelloParam(@PathVariable String strName) {
         return "Hello " + strName + " !!";
     }
 
     @RequestMapping(value = {"/greeting"}, method = RequestMethod.POST)
-    public String sayHelloUser(@RequestBody User user){
+    public String sayHelloUser(@RequestBody User user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName();
+    }
+
+    @PutMapping("/put/{firstName}")
+    public String sayHelloPut(@PathVariable String firstName,
+                              @RequestParam(value = "lastname") String lastName) {
+        return "Hello " + firstName + " " + lastName + " !!";
     }
 
 }
