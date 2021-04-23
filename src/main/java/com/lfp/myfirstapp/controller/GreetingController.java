0b1041c45e682a3c7c4aba70963bd682bc305e16
jayaramvsa <jayaramvsa@gmail.com>
2021-04-23 12:45:11 +0530

    Formatiing

--- src/main/java/com/lfp/myfirstapp/controller/GreetingController.java
+++ src/main/java/com/lfp/myfirstapp/controller/GreetingController.java
@@ -2,17 +2,19 @@ package com.lfp.myfirstapp.controller;
 
 import com.lfp.myfirstapp.dto.response.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
     private static final String strTemplate = "Hello, %s!";
     private final AtomicLong aCounter = new AtomicLong();
 
-    @GetMapping("/greeting")
+    @GetMapping("/hello")
     public Greeting greeting(@RequestParam(value = "name", defaultValue="world") String strName) {
         return new Greeting(aCounter.incrementAndGet(),
                 String.format(strTemplate,strName));
